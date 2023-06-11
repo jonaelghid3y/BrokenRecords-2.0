665ae2a02994089e8b5bbc4507d5df985694a411
jonaelghid3y <jonaelghidey@hotmail.com>
2023-06-11 19:01:35 +0200

    dag 1 jag  är ett odjur

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,81 +1,129 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../components/CartContext';
-import uuid4 from "uuid4";
 import styled from 'styled-components';
 
 const Products = () => {
   const { addToCart } = useContext(CartContext);
   const [productList, setProductList] = useState([]);
   const [visibleProducts, setVisibleProducts] = useState(8);
-  const [allProductsVisible, setAllProductsVisible] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null); // Track the selected category
 
   const handleAddToCart = (productId) => {
-    const productToAdd = productList.find(product => product._id === productId);
+    const productToAdd = productList.find((product) => product._id === productId);
     addToCart(productToAdd);
   };
 
-  const handleFetchProducts = async (category) => {
-    const url = category
-      ? `https://product-api-production-7dbf.up.railway.app/products?category=${category}`
-      : `https://product-api-production-7dbf.up.railway.app/products`;
-
+  const handleFetchProducts = async () => {
+    const url = 'https://product-api-production-7dbf.up.railway.app/products';
     const response = await fetch(url);
     const productList = await response.json();
-    setProductList(productList);
+    return productList;
+  };
+
+  const handleFilterClick = (category) => {
+    setSelectedCategory(category);
   };
 
   const handleSeeMore = () => {
     setVisibleProducts(productList.length);
-    setAllProductsVisible(true);
+  };
+
+  const handleSeeLess = () => {
+    setVisibleProducts(8);
   };
 
   useEffect(() => {
-    handleFetchProducts(); // Fetch all products initially
-  }, []);
-
-  const handleFilterClick = (category) => {
-    handleFetchProducts(category);
-  };
+    handleFetchProducts()
+      .then((products) => {
+        if (selectedCategory) {
+          const filteredProducts = products.filter((product) => product.category === selectedCategory);
+          setProductList(filteredProducts);
+        } else {
+          setProductList(products);
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }, [selectedCategory]);
 
   return (
     <div id="productsdiv">
       <div id="filterbar">
-        <input type='text' placeholder='Search' />
-        <button className='sökknappar'>Sök</button>
+        <input type="text" placeholder="Search" />
+        <button className="sökknappar">Sök</button>
         <h5>Filter:</h5>
-        <button className='sökknappar'  onClick={() => handleFilterClick('Rock')}>Rock</button>
-        <button className='sökknappar' onClick={() => handleFilterClick('Jazz')}>Jazz</button>
-        <button className='sökknappar' onClick={() => handleFilterClick('Hiphop')}>Hiphop</button>
-        <button className='sökknappar' onClick={() => handleFilterClick('Pop')}>Pop</button>
+        <button className="sökknappar" onClick={() => handleFilterClick(null)}>
+          Allt
+        </button>
+        <button className="sökknappar" onClick={() => handleFilterClick('Rock')}>
+          Rock
+        </button>
+        <button className="sökknappar" onClick={() => handleFilterClick('Jazz')}>
+          Jazz
+        </button>
+        <button className="sökknappar" onClick={() => handleFilterClick('Hiphop')}>
+          Hiphop
+        </button>
+        <button className="sökknappar" onClick={() => handleFilterClick('Pop')}>
+          Pop
+        </button>
       </div>
 
       <StyledProductsdiv>
-        <ul className='productUL'>
+        <ul className="productUL">
           {productList.slice(0, visibleProducts).map((product) => (
             <div className="productcard" key={product._id}>
-              <Link to={"/Product/" + product._id}>
-                <div className='productListItem' style={{ backgroundImage: `url(${product.image})` }}>
+              <Link to={`/Product/${product._id}`}>
+                <div className="productListItem" style={{ backgroundImage: `url(${product.image})` }}>
                   {/* Product card content */}
                 </div>
               </Link>
-              <div className='productsinfotext'>
-                <h5 className='albumTitle'>{product.title}</h5>
-                <h4 className='albumInfo'>{product.description}</h4>
-                <h4 className='albumInfo'>Release year: {product.releaseyear}</h4>
-                <p className='priceTag'>{product.price}:-</p>
+              <div className="productsinfotext">
+                <h5 className="albumTitle">{product.title}</h5>
+                <h4 className="albumInfo">{product.description}</h4>
+                <h4 className="albumInfo">Release year: {product.releaseyear}</h4>
+                <p className="priceTag">{product.price}:-</p>
               </div>
-              <Button onClick={() => handleAddToCart(product._id)} className='cartBtn'>Add to cart</Button>
+              <Button onClick={() => handleAddToCart(product._id)} className="cartBtn">
+                Add to cart
+              </Button>
             </div>
           ))}
         </ul>
-        {!allProductsVisible && (
-        <button onClick={handleSeeMore} className='seemorebutton'>See More</button>
-      )}
-
+        {productList.length > 8 && (
+          <div>
+            {visibleProducts < productList.length ? (
+              <button onClick={handleSeeMore} className="seemorebutton">
+                See More
+              </button>
+            ) : (
+              <button onClick={handleSeeLess} className="seemorebutton">
+                See Less
+              </button>
+            )}
+          </div>
+        )}
       </StyledProductsdiv>
-    
+      <div className="reklambanner">
+
+        <h1> SUMMERDEAL use code: SUN för 15% discount!</h1>
+      </div>
+      <div id="presentkortdiv">
+      
+      <div id="Presentkort">
+
+              <div id="presentkortinnehåll">
+              <div id="presentkortkort"></div>
+              <h1 style={{width: '600px',color: 'white'}}>Gift the Soundtrack: Let Music Be Their Personal Symphony</h1>
+              </div>
+      </div>
      
+     
+      </div>
+
+      
     </div>
   );
 };
@@ -92,7 +140,7 @@ const StyledProductsdiv = styled.div`
 `;
 
 const Button = styled.button`
-  color: rgb(241, 198, 6);;
+  color: rgb(241, 198, 6);
   border: 2px solid white;
   background: black;
   border-radius: 5px;

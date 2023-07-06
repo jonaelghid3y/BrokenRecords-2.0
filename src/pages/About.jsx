a45264389b66567ff20e8bceab46675ca6b6d530
jonaelghid3y <jonaelghidey@hotmail.com>
2023-07-06 11:39:02 +0200

    ok

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,11 @@ export default function About() {
   return (
     <div  id="aboutdiv">
       <div id="parallax"></div>
-      <div id="parallaxtextdiv">paralax text</div>
+      <div id="parallaxtextdiv">
+        <h2>About us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere vero alias delectus sit tenetur corrupti illum magni dicta quisquam aliquam, eius quod perferendis, consequuntur illo velit dolores odit expedita.</p>
+        
+      </div>
       <div id="parallax2"></div>
 
 

6c926b6d9c10f27bcbafb7205f8acd69c2db575c
DanielNevadoK <113299772+DanielNevadoK@users.noreply.github.com>
2023-07-08 16:17:35 +0200

    footer fam

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,144 +1,64 @@
-import React from 'react'
-import styled from 'styled-components'
-import {AiOutlineInstagram} from 'react-icons/ai'
-import {AiOutlineTwitter} from 'react-icons/ai'
-import {AiFillFacebook} from 'react-icons/ai'
-import {RiSpotifyFill} from 'react-icons/ri'
-import {TbTrademark} from 'react-icons/tb'
-import {SiMastercard} from 'react-icons/si'
-import {SiKlarna} from 'react-icons/si';
-import {SiPaypal} from 'react-icons/si'
+import React from 'react';
+import styled from 'styled-components';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillFacebook } from 'react-icons/ai';
+import { RiSpotifyFill } from 'react-icons/ri';
+import { TbTrademark } from 'react-icons/tb';
+import { SiMastercard } from 'react-icons/si';
+import { SiKlarna } from 'react-icons/si';
+import { SiPaypal } from 'react-icons/si';
 
 const Footer = () => {
   return (
-    <FooterTop>
-      <FooterContent className="footerContent">
-        <div className="logoContainer">
-          <h1>
-          <TbTrademark size={20} color='white'/> BROKEN RECORDS
+    <div class="FooterTop">
+      <div class="FooterContent">
+        <div class="logoContainer">
+          <h1 className='footerheadline'>
+            <TbTrademark size={20} color='white'/> BROKEN RECORDS
           </h1>
         </div>
-        <div>
-          <CustomerService>
-            <h3>
-              Costumer service
-            </h3>
+        <div className='phone-container'>
+          <div class="CustomerService">
+            <div> 
+              <h3>
+                Customer service
+              </h3> 
+            </div>
             <p>Contact us</p>
             <p>Order status</p>
             <p>Privacy policy & Cookies</p>
             <p>Return & QA</p>
-          </CustomerService>
-        </div>
-        {/* <div>
-        <ExtraSpace>
-          Pay with
-        </ExtraSpace>
-          <PaymentContainer>
-            <SiMastercard color='white' size={25}/>
-            <SiKlarna color='white' size={25}/>
-            <SiPaypal color='white' size={25}/>
-          </PaymentContainer>
-        </div>
-        <div>
-          <div className='socialHeadline'>
-            <ExtraSpace>
-              Find us at
-            </ExtraSpace>
           </div>
-          <SocialLogos>
-            <AiOutlineInstagram size={25}/>
-            <AiOutlineTwitter size={25} />
-            <AiFillFacebook size={25}/>
-            <RiSpotifyFill size={25}/>
-          </SocialLogos>
-        </div> */}
-      </FooterContent>   
-    </FooterTop >
-
-  )
+          <div className='rightContainer'>
+            <div className='payHeadline'>
+              <h3 class="ExtraSpace">
+                Pay with
+              </h3>
+                <div class="PaymentContainer">
+                  <SiMastercard  />
+                  <SiKlarna  />
+                  <SiPaypal  />
+                </div>
+            </div>
+            <div class = 'socialContainer'>
+                <div class='socialHeadline'>
+                  <h3 class="ExtraSpace">
+                    Find us at
+                  </h3>
+                </div>
+              <div class="SocialLogos">
+                <AiOutlineInstagram />
+                <AiOutlineTwitter  />
+                <AiFillFacebook />
+                <RiSpotifyFill />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>   
+    </div>
+  );
 }
 
-const FooterTop = styled.div`
-  background: black;
-  display: flex;
-  width:100%
-  justify-content: center;
-  aline-itenms: center;
-  flex-direction: row;
-  background-color: black;
-  @media (max-width: 768px) {
-    font-size: 15px;
-    
-  }
-`;
-
-const FooterContent = styled.div`
-
-  padding:3vw;
-  width: 100%;
-  height: 180px;
-  color: white;
-  display: flex;
-  justify-content: space-between;
-`;
-
-const CustomerService = styled.ul`
-  display: flex;
-  flex-direction: column;
-
-  & > * {
-    margin-top: 5px;
-    text-align: left;
-  }
-  &:hover {
-    cursor: pointer;
-  }
-`;
-
-
-const SocialLogos = styled.div`
-display: flex;
-
-& > * {
-  margin: 20px;
-  margin-top:10px;
-  text-align: left;
-}
-&:hover {
-  cursor: pointer;
-}
-& > * {
-  margin-right: 10px;
-}
-& > svg:nth-of-type(1):hover {
-  fill: pink;
-}
-
-& > svg:nth-of-type(2):hover {
-  fill: #00acee;
-}
-
-& > svg:nth-of-type(3):hover {
-  fill: #3b5998;
-}
-
-& > svg:nth-of-type(4):hover {
-  fill: limegreen;
-}
-
-`;
-
-const PaymentContainer = styled.div`
-display: flex;
-
-& > * {
-  margin: 20px;
-  margin-top:10px;
-  text-align: left;
-}
-`;
-const ExtraSpace = styled.h3`
-  // margin-left: 20px;
-`;
-
-export default Footer
+export default Footer;
